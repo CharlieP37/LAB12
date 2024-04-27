d81e389b3878c2c3c42b3036d3f18d411aa22d63
AndyGil-03 <71861205+AndyGil-03@users.noreply.github.com>
2024-04-26 19:14:04 -0600

    Views chat and normal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,16 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult GameMain()
+    {
+        return View();
+    }
+
+    public IActionResult GameChat()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
